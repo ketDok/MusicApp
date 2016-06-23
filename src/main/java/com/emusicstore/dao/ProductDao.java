da3553da538df1b29c975fae4a6f0f91ca669db1
Ketkee <ksmaknikar@gmail.com>
2016-06-22 23:10:14 -0400

    leran17

--- src/main/java/com/emusicstore/dao/ProductDao.java
+++ src/main/java/com/emusicstore/dao/ProductDao.java
@@ -11,7 +11,6 @@ public class ProductDao {
 
     public List<Product> getProductList() {
         Product product1 = new Product();
-
         product1.setProductName("Guitar1");
         product1.setProductCategory("Instrument");
         product1.setProductDescription("This is a fender strat guitar!");
@@ -21,8 +20,21 @@ public class ProductDao {
         product1.setUnitInStock(11);
         product1.setProductManufacturer("Fender");
 
+        Product product2 = new Product();
+        product2.setProductName("Guitar2");
+        product2.setProductCategory("Instrument");
+        product2.setProductDescription("This is a fender strat guitar!");
+        product2.setProductPrice(1200);
+        product2.setProductCondition("new");
+        product2.setProductStatus("Active");
+        product2.setUnitInStock(11);
+        product2.setProductManufacturer("Fender");
+
+
+
         productList=new ArrayList<Product>();
         productList.add(product1);
+        productList.add(product2);
 
         return productList;
     }
